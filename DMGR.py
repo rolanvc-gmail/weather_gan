1b4f56c75ed7853b32aa947a1bb70dcb48a45c2d
Rolan Veron Cruz <rolanvc@gmail.com>
2022-06-22 16:10:21 +0800

    DMGR code ok

--- DMGR.py
+++ DMGR.py
@@ -1,15 +1,15 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
-from losses.dmgr_losses import NowcastingLoss, GridCellLoss
+from losses.dmgr_losses import NowcastingLoss, GridCellLoss, loss_hinge_disc, grid_cell_regularizer, loss_hinge_gen
 from ConditioningStack import ConditioningStack
 from LatentConditioningStack import LatentConditioningStack
 from Sampler import Sampler
 from Generator import Generator
 from Discriminator import Discriminator
+import pytorch_lightning as pl
 
 
-class DGMR(nn.Module):
+class DGMR(pl.LightningModule):
     """
     Deep Generative Model of Radar
     """
@@ -48,8 +48,8 @@ class DGMR(nn.Module):
         self.disc_lr = disc_lr
         self.beta1 = beta1
         self.beta2 = beta2
-        self.discriminator_loss = NowcastingLoss()
-        self.grid_regularizer = GridCellLoss()
+        self.discriminator_loss = NowcastingLoss()  # Todo : figure this out.
+        self.grid_regularizer = GridCellLoss()  # Todo : figure this out.
         self.grid_lambda = grid_lambda
         self.num_samples = num_samples
         self.latent_channels = latent_channels
@@ -72,11 +72,69 @@ class DGMR(nn.Module):
         self.automatic_optimization = False
         torch.autograd.set_detect_anomaly(True)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.generator(x)
         return x
 
     def training_step(self, batch, batch_idx):
-        
+        images, future_images = batch
+        images = images.float()
+        future_images = future_images.float()
+        self.global_iteration += 1
+        g_opt, d_opt = self.optimizers()
+
+        # Two discriminator steps per generator step
+        for _ in range(2):
+            predictions = self(images)
+            # cat along the time dimension [B, T, C, H, W]
+            generated_sequences = torch.cat([images, predictions], dim=1)
+            real_sequence = torch.cat([images, future_images], dim=1)
+            # cat along batch for real+generated
+            concatenated_inputs = torch.cat([real_sequence, generated_sequences], dim=0)
+
+            concatenated_outputs = self.discriminator(concatenated_inputs)
+            score_real, score_generated = torch.split(concatenated_outputs, 1, dim=1)
+            discriminator_loss = loss_hinge_disc(score_generated, score_real)  # Todo: understand this loss function.
+            d_opt.zero_grad()
+            self.manual_backward(discriminator_loss)  # manual_backward is from pytorch lightning
+            d_opt.step()
+
+        # Optimize generator
+        predictions = [self(images) for _ in range(6)]
+        grid_cell_reg = grid_cell_regularizer(torch.stack(predictions, dim=0), future_images)
+        # Concat along time dimension
+        generated_sequences = [torch.cat([images, x], dim=1) for x in predictions]
+        real_sequence = torch.cat([images, future_images], dim=1)
+        # cat long batch for the real+generated, for each example in the range
+        # For each of the 6 examples
+        generated_scores = []
+        for g_seq in generated_sequences:
+            concatenated_inputs = torch.cat([real_sequence, g_seq], dim=0)
+            concatenated_outputs = self.discriminator(concatenated_inputs)
+            score_real, score_generated = torch.split(concatenated_outputs, 1, dim=1)
+            generated_scores.append(score_generated)
+
+        generator_disc_loss = loss_hinge_gen(torch.cat(generated_scores, dim=0))
+        generator_loss = generator_disc_loss + self.grid_lambda * grid_cell_reg
+        g_opt.zero_grad()
+        self.manual_backward(generator_loss)   # manual_backward is from pytorch lightning
+        g_opt.step()
+
+        generated_images = self(images)
+
+    def configure_optimizers(self):
+        b1 = self.beta1
+        b2 = self.beta2
+
+        opt_g = torch.optim.Adam(self.generator.parameters(), lr=self.gen_lr, betas=(b1, b2))
+        opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=self.disc_lr, betas=(b1, b2))
+
+        return [opt_g, opt_d], []
+
+
+
+
+
+
 
 
--- losses/dmgr_losses.py
+++ losses/dmgr_losses.py
@@ -34,3 +34,33 @@ class GridCellLoss(nn.Module):
 
         difference /= targets.size(1)*targets.size(3) * targets.size(4)  # 1/HWN
         return difference.mean()
+
+
+def loss_hinge_disc(score_generated, score_real):
+    """ Discriminator hinge loss."""
+    l1 = F.relu(1.0 - score_real)
+    loss = torch.mean(l1)
+    l2 = F.relu(1.0 + score_generated)
+    loss += torch.mean(l2)
+    return loss
+
+
+def grid_cell_regularizer(generated_samples, batch_targets):
+    """
+    Grid cell regularizer
+    :param generated_samples: Tensor of size [n_samples, batch_size, 18, 256,256, 1].
+    :param batch_targets: Tensor of size [batch_size, 18, 256, 256, 1].
+    :return loss: a tensor of shape [batch_size].
+    """
+    gen_mean = torch.mean(generated_samples, dim=0)
+    weights = torch.clip(batch_targets, 0.0, 24.0)
+    loss = torch.mean(torch.abs(gen_mean - batch_targets)* weights)
+    return loss
+
+
+def loss_hinge_gen(score_generated):
+    """
+    Generator hinge loss
+    """
+    loss = -torch.mean(score_generated)
+    return loss
