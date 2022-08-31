9df9d0f2f6b831d3ea435bc5f9685d659925efc1
Rolan Veron Cruz <rolanvc@gmail.com>
2022-08-31 19:48:56 +0800

    removed print() statement

--- alDMGR.py
+++ alDMGR.py
@@ -93,9 +93,7 @@ class AlDGMR(nn.Module):
             s_sd = random.sample(range(0, 18), 8)
 
             z = Variable(Tensor(np.random.normal(0, 1, (16, 8, 8, 8))))  # latent variable input for latent conditioning stack
-            print("Generating predictions")
             predictions = self.generator(images_data, z)  # predictions should be 16x18x256x256x1
-            print("preditions's shape is: {}".format(predictions.shape))
             sd_score_predictions = self.spatial_discriminator(predictions[:, s_sd])  # we only use 8 of 18 images to get sd_score, sd_score should be 16x1x1
             sd_score_target_images = self.spatial_discriminator(target_images[:, s_sd])
             sd_loss = torch.mean(nn.ReLU()(1-sd_score_target_images) + nn.ReLU()(1+sd_score_predictions))
--- al_generator.py
+++ al_generator.py
@@ -46,9 +46,7 @@ class AlGenerator(nn.Module):
         # LCS_output is Tensor(batch_size, 1, 768, 8,8)
         LCS_outputs = [LCS_output] * 18
         output = self.sampler(LCS_outputs, CD_output)
-        print("generator output shape is:{}".format(output.shape))
         output = torch.unsqueeze(output, 2)
-        print("generator output shape is:{}".format(output.shape))
         return output
 
 
